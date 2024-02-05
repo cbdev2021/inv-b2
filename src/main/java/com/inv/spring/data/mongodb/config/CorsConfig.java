d3a4272d538928eb7698b25962e2cda411ca479f
carlos <cbdev2021@gmail.com>
2024-02-05 17:56:42 -0300

    roll back localhost ports

--- src/main/java/com/inv/spring/data/mongodb/config/CorsConfig.java
+++ src/main/java/com/inv/spring/data/mongodb/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3004, https://localhost:3004")
+                .allowedOrigins("http://localhost:3000, https://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
